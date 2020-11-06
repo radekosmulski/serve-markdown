bc1994c4fd498c25f874fa84bb14f228cd1225ce
Radek Osmulski <rosmulski@gmail.com>
2020-11-06 16:07:36 +0100

    add regenaration of html on md file change

--- lib/serve-markdown.rb
+++ lib/serve-markdown.rb
@@ -4,6 +4,7 @@ require 'webrick'
 require 'optparse'
 require 'pathname'
 require 'fileutils'
+require 'listen'
 
 @options = {}
 OptionParser.new do |opts|
@@ -17,20 +18,27 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-FileUtils.remove_dir('html', force=true)
-FileUtils.mkdir('html')
-
 layout_path = Pathname.new('templates/layout.html.erb').exist? ? 'templates/layout.html.erb' : File.join(__dir__, 'templates/layout.html.erb')
-
 layout = Tilt.new(layout_path)
-filenames = Dir['*.md'].map { |fn| fn[/(.*)\.md/, 1] } # removes the .md suffix from fns
 
-filenames.map { |fn| ["#{fn}.md", "#{fn}.html"] }.each do |md_fn, html_fn|
-  contents = Tilt.new(md_fn).render
-  File.open(File.join('html', html_fn), 'w') do |file|
-    file.write layout.render(Object.new, contents: contents, filenames: filenames)
+generate_html = Proc.new do
+  FileUtils.remove_dir('html', force=true)
+  FileUtils.mkdir('html')
+
+  filenames = Dir['*.md'].map { |fn| fn[/(.*)\.md/, 1] } # removes the .md suffix from fns
+
+  filenames.map { |fn| ["#{fn}.md", "#{fn}.html"] }.each do |md_fn, html_fn|
+    contents = Tilt.new(md_fn).render
+    File.open(File.join('html', html_fn), 'w') do |file|
+      file.write layout.render(Object.new, contents: contents, filenames: filenames)
+    end
   end
 end
+
+listener = Listen.to('.') do |modified, added ,removed|
+  generate_html.call
+end
+listener.start
 
 root = File.expand_path 'html'
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
--- serve-markdown.gemspec
+++ serve-markdown.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'serve-markdown'
-  s.version     = '0.0.3'
+  s.version     = '0.0.4'
   s.date        = '2020-11-03'
   s.summary     = "Serve markdown files from current directory on localhost."
   s.description = "A simple solution for serving markdown files from current directory."
@@ -16,4 +16,5 @@ Gem::Specification.new do |s|
   s.add_dependency 'tilt', '~> 2.0'
   s.add_dependency 'pandoc-ruby', '~> 2.1'
   s.add_dependency 'webrick', '~> 1.6'
+  s.add_dependency 'listen', '~> 3.2'
 end
